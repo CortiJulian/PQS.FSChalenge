541a849e9523c98d95ffe4a41ffb305f71ced8bc
Julián Corti <julian.1993@live.com.ar>
2020-09-06 17:31:55 -0300

    Create Front End

---
+++ insert.sql
@@ -0,0 +1,11 @@
+insert into ORDERS (OrderStatus, OrderDescription)
+values (0, 'test')
+
+insert into ORDER_ITEMS (OrderId, Quantity, UnitPrice, ItemDescription)
+values (2, 800, 1.66, 'adads')
+
+select * from ORDERS
+
+select * from ORDER_ITEMS
+
+select * from vORDERS_INFO
