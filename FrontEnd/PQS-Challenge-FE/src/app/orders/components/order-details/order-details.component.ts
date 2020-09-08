cd58b9cff284ba3f12b603f2b04c43b987fe5aaa
Julián Corti <julian.1993@live.com.ar>
2020-09-08 02:46:37 -0300

    order details

--- FrontEnd/PQS-Challenge-FE/src/app/orders/components/order-details/order-details.component.ts
+++ FrontEnd/PQS-Challenge-FE/src/app/orders/components/order-details/order-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
   selector: 'app-order-details',
@@ -10,15 +11,35 @@ export class OrderDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private orderService: OrdersService
   ) {}
 
   orderId: number;
+  order: any;
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      this.orderId = +params['id'];
-      console.log('param', this.orderId);
-    });
+      this.orderId = Number.parseInt(this.route.snapshot.paramMap.get('id'), 10);
+      this.orderService.getOrderById(this.orderId).subscribe(result => {
+        this.order = result;
+      });
+  }
+
+  approveOrder() {
+    this.orderService.approveOrder(this.orderId).subscribe(result => {
+      console.log('result:', result);
+    },
+    error => {
+      console.error(error);
+    })
+  }
+
+  rejectOrder() {
+    this.orderService.rejectOrder(this.orderId).subscribe(result => {
+      console.log('result:', result);
+    },
+    error => {
+      console.error(error);
+    })
   }
 
 }
--- FrontEnd/PQS-Challenge-FE/src/app/orders/services/orders.service.ts
+++ FrontEnd/PQS-Challenge-FE/src/app/orders/services/orders.service.ts
@@ -9,10 +9,18 @@ export class OrdersService {
   constructor(protected http: HttpClient) { }
 
   getOrderById(orderId: number) {
-    return this.http.get('http://localhost:53634/api/Orders/GetOrderById/' + orderId);
+    return this.http.get('http://localhost:53634/api/Orders/' + orderId);
   }
 
   getOrders(status: string) {
     return this.http.get('http://localhost:53634/api/Orders/' + status);
+  }
+
+  approveOrder(id: number) {
+    return this.http.post('http://localhost:53634/api/Orders/' + id, '');
+  }
+
+  rejectOrder(id: number) {
+    return this.http.delete('http://localhost:53634/api/Orders/' + id);
   }
 }
